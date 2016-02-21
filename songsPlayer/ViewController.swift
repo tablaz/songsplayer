88810a71796d05c9bcdc89bb683d42f9f8af57b0
Ricardo Restrepo M <rrestrepo@seeketing.com>
2016-02-21 20:01:02 +0000

    Initial Commit

---
+++ songsPlayer/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  songsPlayer
+//
+//  Created by Ricardo on 21/02/2016.
+//  Copyright © 2016 Tablaz. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
